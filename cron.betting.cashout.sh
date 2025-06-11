91a8acc24c01ad6fd850b4cf5562b34b05ff37c0
Developer Phahla <dev.phahla@gmail.com>
2025-06-12 00:04:28 +0200

    create cron jobs

--- cron.betting.cashout.sh
+++ cron.betting.cashout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
     if ! pgrep -f run_gbets_cashout.py >/dev/null; then
-        cd /Users/g0d/Workspace/projects/betting;
+        cd /home/g0d/Workspace/Projects/betting/;
         source venv/bin/activate;
         python run_gbets_cashout.py;
     fi
